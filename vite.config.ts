f22ae4dfb5031d5c15ca058b34522b78d0f9f662
feldrone <fellah.amine2050@gmail.com>
2026-09-11 23:46:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,8 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const dirname = path.dirname(filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
